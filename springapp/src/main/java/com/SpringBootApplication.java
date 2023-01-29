e4aa29cd1e46625eec76004b06709e2bdde10313
elakkiy2001 <ELAKKIYAELAN2001@GMAIL.COM>
2023-01-29 06:49:14 +0000

    first

--- springapp/src/main/java/com/SpringBootApplication.java
+++ springapp/src/main/java/com/SpringBootApplication.java
@@ -4,14 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-@SpringBootApplication
-@EnableJpaRepositories("dao")
+ @SpringBootApplication
+ @EnableJpaRepositories("dao")
 @ComponentScan("controller")
-@EntityScan("model")
-public class BloodBankApplication {
-
+ @EntityScan("model")
+public class SpringBootApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(BloodBankApplication.class, args);
+		SpringApplication.run(SpringBootApplication.class, args);
 	}
 
 }
